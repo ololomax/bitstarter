3fca2f0ce57764f96c77d21c5cf1cd4637014ca9
ololomax <ghostxm@yandex.ru>
2013-07-10 22:34:49 +0000

    next commit

--- web.js
+++ web.js
@@ -6,8 +6,8 @@ app.get('/', function(request, response) {
   var fs = require('fs');
   fs.readFileSync('index.html', function (err, data) {
       if (err) throw err;
-      var buffer = new Buffer(data);
-      response.send(data));
+      //var buffer = new Buffer(data);
+      response.send(data.toString()));
   });
   //response.send('sasai)))');
 });
