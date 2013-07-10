efb083eee1084c4bf89a881e6513ed9b737f7dfc
ololomax <ghostxm@yandex.ru>
2013-07-10 23:37:01 +0000

    next commit

--- web.js
+++ web.js
@@ -2,19 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer('loh');
-
 app.get('/', function(request, response) {
   var fs = require('fs');
-  fs.readFileSync('index.html', 'utf8', function (err, data) {
-      if (err) throw err;
-      buffer = new Buffer(data);
-      console.log(data);
-      //response.send(data.toString()));
-      //response.send('azaza');
-  });
+  var buffer = new Buffer(fs.readFileSync('index.html', 'utf8'));
   response.send(buffer.toString());
-  console.log('1');
 });
 
 var port = process.env.PORT || 5000;
